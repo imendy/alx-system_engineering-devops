50df3a4f04f0f5ceb2c15620fb3c6e32c8789ed5
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-09-27 17:38:40 -0700

    completed task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,16 @@
-# Puppet script to create ssh config file
+# Seting up my client config file
+include stdlib
+
 file_line { 'Turn off passwd auth':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
-  ensure => 'present',
+file_line { 'Delare identity file':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
