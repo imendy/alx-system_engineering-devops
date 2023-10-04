ffb8ad26c039c22933462e80d94ac757e24eab6c
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-10-03 19:10:57 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,7 +15,7 @@ file_line { 'redirect_me':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://github.com/imendy permanent;',
 }
 
 service { 'nginx':
