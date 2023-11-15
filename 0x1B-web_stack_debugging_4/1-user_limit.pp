d9f2bf6b1cbe446be95dab47d5af36932b1e267d
Emmanuel Ndukwe Nmaju <enmaju37@gmail.com>
2023-11-14 18:48:46 -0800

    feat: web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,17 @@
+# Create user holberton
+user { 'holberton':
+  ensure => 'present',
+  managehome => true,
+}
+
+# Increase hard file limit for user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
